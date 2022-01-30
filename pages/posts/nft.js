e78bb4f2c6285bbf2522ce988b4d17f44f2c6598
shreyjain13 <shreyd.jain@mail.utoronto.ca>
2022-01-30 18:16:54 -0500

    new

--- pages/posts/nft.js
+++ pages/posts/nft.js
@@ -7,6 +7,7 @@ export default function FirstPost() {
     <h1><center>NFT Articles</center></h1>
     <h2><center>Article #1: <a href="https://shreyj.com"><u>Here</u></a></center></h2>
     <p><center> Go back to the home page to sign!</center></p>
+    <p><center>These will all be Mirror articles published on the Arweave blockchain associated with my wallet address.</center></p>
     </>
 
 
